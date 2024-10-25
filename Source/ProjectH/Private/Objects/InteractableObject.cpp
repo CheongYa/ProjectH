e5eb282727a7e7addf392c051e787151556c45e5
Hospiea <155417815+Hospiea@users.noreply.github.com>
2024-10-26 06:08:36 +0900

    FIX: BP

--- Source/ProjectH/Private/Objects/InteractableObject.cpp
+++ Source/ProjectH/Private/Objects/InteractableObject.cpp
@@ -56,7 +56,7 @@ void AInteractableObject::BeginPlay()
 void AInteractableObject::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	
 }
 
 void AInteractableObject::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
